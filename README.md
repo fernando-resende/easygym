55624a67543611053a6d12cbafb7e88cbf7d9295
Fernando Resende <fernandohrcampos@yahoo.com.br>
2022-08-20 14:57:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Easy Gym
+
+## About
+
+⚠️**Warning**⚠️: This is a **hobby project** just for test some Spring Boot things.
+
+## Objective
+
+Expose some APIs related to a gym like client and exercises.
---
+++ src/test/java/personal/project/easygym/EasygymApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.project.easygym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasygymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
